c1cf7e025ff7a3a33273740832f2e6587fe35200
ShevaKuilin <shevakuilin@gmail.com>
2018-01-04 10:59:04 +0800

    creat project

---
+++ No Wait/No Wait/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  No Wait
+//
+//  Created by ShevaKuilin on 2018/1/4.
+//  Copyright © 2018年 ShevaKuilin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
